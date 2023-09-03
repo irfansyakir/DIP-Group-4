809394cd7ef58dc5d94a34b68d69401e4ef851fc
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-09-03 21:35:37 +0800

    [Auth] Set up navigations and Zustand stores

--- src/App/index.js
+++ src/App/index.js
@@ -1,26 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import * as React from 'react';
-import {Fragment} from "react";
-import {Navigation} from "../Navigation";
+import * as React from 'react'
+import { Fragment } from 'react'
+import { Navigation } from '../Navigation'
 
-
-const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 export const AppContainer = () => {
-    return (
-        <Fragment>
-            <Navigation/>
-        </Fragment>
-    );
+  return (
+    <Fragment>
+      <Navigation />
+    </Fragment>
+  )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'pink',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
