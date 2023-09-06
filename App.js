0a89f795c171e42e57fc5025727575322e81f4ef
Azuoraaa <mapleace98@gmail.com>
2023-09-06 14:21:38 +0800

    For Irfan

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import {AppContainer} from "./src/App";
-// yellow is the default color for the container element and the container element itself is black because it is
+// Blue and yellow is the default color for the container element and the container element itself is black because it is
 export default function App() {
   return (
     <AppContainer/>
