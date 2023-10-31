4f29d49c340004e0048e32981548d4a653291f20
Gabrielle Nicole Suharjono <gabrielle.nicole2003@gmail.com>
2023-11-01 01:27:04 +0800

    feat: update login page and music player

--- src/Commons/UI/currentlyPlaying.js
+++ src/Commons/UI/currentlyPlaying.js
@@ -6,150 +6,172 @@ import Ionicons from '@expo/vector-icons/Ionicons'
 import { useMusicStore } from '../../Store/useMusicStore'
 import { useEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import {useSafeAreaInsets} from "react-native-safe-area-context";
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 const SongProgessBar = ({ currentTime, duration }) => {
-  return (
-    <View
-      style={{
-        height: 4,
-        backgroundColor: '#100D22',
-        borderRadius: 3,
-      }}
-    >
-      <View
-        style={{
-          width: `${(currentTime / duration) * 100}%`,
-          height: '100%',
-          backgroundColor: COLORS.light,
-          borderRadius: 3,
-        }}
-      />
-    </View>
-  )
+    return (
+        <View
+            style={{
+                height: 4,
+                backgroundColor: '#100D22',
+                borderRadius: 3,
+            }}
+        >
+            <View
+                style={{
+                    width: `${(currentTime / duration) * 100}%`,
+                    height: '100%',
+                    backgroundColor: COLORS.light,
+                    borderRadius: 3,
+                }}
+            />
+        </View>
+    )
 }
 
 export function CurrentlyPlaying({ currentPage }) {
-  const screenWidth = Dimensions.get('window').width
-  const songInfo = useMusicStore((state) => state.songInfo)
-  const isPlaying = useMusicStore((state) => state.isPlaying)
-  const soundObject = useMusicStore((state) => state.soundObject)
-  const changeIsPlaying = useMusicStore((state) => state.changeIsPlaying)
-  const changeCurrentPage = useMusicStore((state) => state.changeCurrentPage)
-  const [posInterval, setPosInterval] = useState()
-  const [position, setPosition] = useState()
-  const [duration, setDuration] = useState()
-  const navigation = useNavigation()
-  const insets = useSafeAreaInsets()
+    const screenWidth = Dimensions.get('window').width
+    const songInfo = useMusicStore((state) => state.songInfo)
+    const isPlaying = useMusicStore((state) => state.isPlaying)
+    const soundObject = useMusicStore((state) => state.soundObject)
+    const changeIsPlaying = useMusicStore((state) => state.changeIsPlaying)
+    const changeCurrentPage = useMusicStore((state) => state.changeCurrentPage)
+    const position = useMusicStore((state) => state.position)
+    const changePosition = useMusicStore((state) => state.changePosition)
+    const duration = useMusicStore((state) => state.duration)
+    const changeDuration = useMusicStore((state) => state.changeDuration)
+    const navigation = useNavigation()
+    const insets = useSafeAreaInsets()
 
-  const play = async () => {
-    try {
-      await soundObject.playAsync()
-    } catch (err) {
-      console.log(err)
+    const play = async () => {
+        try {
+            await soundObject.playAsync()
+        } catch (err) {
+            console.log(err)
+        }
     }
-  }
 
-  const pause = async () => {
-    try {
-      await soundObject.pauseAsync()
-    } catch (err) {
-      console.log(err)
+    const pause = async () => {
+        try {
+            await soundObject.pauseAsync()
+        } catch (err) {
+            console.log(err)
+        }
     }
-  }
 
-  const updatePosition = async () => {
-    if (soundObject) {
-      const status = await soundObject.getStatusAsync()
-      setPosition(status.positionMillis)
-      setDuration(status.durationMillis)
-      if (!status.isPlaying) clearInterval(posInterval)
+    const updatePosition = async (intervalId) => {
+        if (soundObject) {
+            const status = await soundObject.getStatusAsync()
+            changePosition(status.positionMillis)
+            changeDuration(status.durationMillis)
+
+            if (status.positionMillis > status.durationMillis - 40) {
+                clearInterval(intervalId)
+                changeIsPlaying(false)
+                await soundObject.setPositionAsync(0)
+                changePosition(0)
+            }
+        }
     }
-  }
 
-  useEffect(() => {
-    // Periodically update the playback position
-    setPosition(0)
-    clearInterval(posInterval)
-    setPosInterval(setInterval(updatePosition, 1000))
+    useEffect(() => {
+        if (soundObject && isPlaying) {
+            // Periodically update the playback position
+            const intervalId = setInterval(
+                () => updatePosition(intervalId),
+                500
+            )
+            // Clean up the interval when the component unmounts
+            return () => clearInterval(intervalId)
+        }
+    }, [soundObject, isPlaying])
 
-    // Clean up the interval when the component unmounts
-    return () => clearInterval(posInterval)
-  }, [soundObject])
+    useEffect(() => {
+        if (soundObject) {
+            isPlaying ? play() : pause()
+        }
+    }, [isPlaying])
 
-  useEffect(() => {
-    if (soundObject) {
-      isPlaying ? play() : pause()
-    }
-  }, [isPlaying])
-
-  return !soundObject ? null : (
-    <Pressable
-      style={{
-        position: 'absolute',
-        width: screenWidth - 20,
-        left: 10,
-        right: 10,
-        height: 70,
-        bottom: insets.bottom + 60,
-        backgroundColor: '#303847',
-        borderRadius: 10,
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingHorizontal: 20,
-        paddingVertical: 10,
-        display: currentPage === 'Track' ? 'none' : 'flex',
-      }}
-      onPress={() => {
-        navigation.navigate('Track')
-        changeCurrentPage('Track')
-      }}
-    >
-      <Image style={{ width: 50, height: 50 }} src={songInfo.coverUrl} />
-      <View
-        aria-label='text and bar'
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          flex: 1,
-          height: '100%',
-          paddingLeft: 10,
-          justifyContent: 'space-around',
-        }}
-      >
-        <View
-          aria-label='text and play button box'
-          style={{ display: 'flex', flexDirection: 'row' }}
-        >
-          <View
-            aria-label='text box'
+    return !soundObject ? null : (
+        <Pressable
             style={{
-              flexGrow: 1,
-              display: 'flex',
+                position: 'absolute',
+                width: screenWidth - 20,
+                left: 10,
+                right: 10,
+                height: 70,
+                bottom: insets.bottom + 60,
+                backgroundColor: '#303847',
+                borderRadius: 10,
+                flexDirection: 'row',
+                alignItems: 'center',
+                paddingHorizontal: 20,
+                paddingVertical: 10,
+                display: currentPage === 'Track' ? 'none' : 'flex',
             }}
-          >
-            <MediumText style={{ color: 'white', fontSize: 14 }}>
-              {songInfo.songTitle}
-            </MediumText>
-            <LightText style={{ color: COLORS.light, fontSize: 12 }}>
-              {songInfo.songArtist}
-            </LightText>
-          </View>
-          <TouchableOpacity
             onPress={() => {
-              changeIsPlaying(!isPlaying)
+                navigation.navigate('Track')
+                changeCurrentPage('Track')
             }}
-          >
-            {/* update state for pause and play */}
-            {!isPlaying ? (
-              <Ionicons name='play' size={24} color={COLORS.white} />
-            ) : (
-              <Ionicons name='pause' size={24} color={COLORS.white} />
-            )}
-          </TouchableOpacity>
-        </View>
-        <SongProgessBar currentTime={position} duration={duration - 39} />
-      </View>
-    </Pressable>
-  )
+        >
+            <Image style={{ width: 50, height: 50 }} src={songInfo.coverUrl} />
+            <View
+                aria-label='text and bar'
+                style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    height: '100%',
+                    paddingLeft: 10,
+                    justifyContent: 'space-around',
+                }}
+            >
+                <View
+                    aria-label='text and play button box'
+                    style={{ display: 'flex', flexDirection: 'row' }}
+                >
+                    <View
+                        aria-label='text box'
+                        style={{
+                            flexGrow: 1,
+                            display: 'flex',
+                        }}
+                    >
+                        <MediumText style={{ color: 'white', fontSize: 14 }}>
+                            {songInfo.songTitle}
+                        </MediumText>
+                        <LightText
+                            style={{ color: COLORS.light, fontSize: 12 }}
+                        >
+                            {songInfo.songArtist}
+                        </LightText>
+                    </View>
+                    <TouchableOpacity
+                        onPress={() => {
+                            changeIsPlaying(!isPlaying)
+                        }}
+                    >
+                        {/* update state for pause and play */}
+                        {!isPlaying ? (
+                            <Ionicons
+                                name='play'
+                                size={24}
+                                color={COLORS.white}
+                            />
+                        ) : (
+                            <Ionicons
+                                name='pause'
+                                size={24}
+                                color={COLORS.white}
+                            />
+                        )}
+                    </TouchableOpacity>
+                </View>
+                <SongProgessBar
+                    currentTime={position}
+                    duration={duration - 39}
+                />
+            </View>
+        </Pressable>
+    )
 }
--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,60 +1,94 @@
 import { Button } from '@rneui/themed'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { useAuthStore } from '../../Store/useAuthStore'
 import { useSpotifyAuthenticate } from '../../Utilities/SpotifyApi/useSpotifyAuthenticate'
-import { useEffect, useState, useRef } from 'react';
-import { GetCurrentUserProfile} from '../../Utilities/SpotifyApi/Utils'
+import { useEffect, useState, useRef } from 'react'
+import { GetCurrentUserProfile } from '../../Utilities/SpotifyApi/Utils'
+import { BoldText } from '../../Commons/UI/styledText'
+import { Box } from '@rneui/layout'
+import { LinearGradient } from 'expo-linear-gradient'
+import { View, Image } from 'react-native'
+import { COLORS } from '../../Constants'
 
 export const Login = () => {
-  const changeIsLoggedIn = useAuthStore((state) => state.changeIsLoggedIn)
-  const [apiLogin] = useSpotifyAuthenticate()
+    const changeIsLoggedIn = useAuthStore((state) => state.changeIsLoggedIn)
+    const [apiLogin] = useSpotifyAuthenticate()
 
-  // TODO:
-  // - add redirect to Spotify Web
-  // - pass userId back to app
-  // - update userId state => changeUserId(userId)
-  // - update isLoggedIn state => changeIsLoggedIn(true)
-  // optional: add 'you're being redirected to spotify' page, 'failed login' page
+    // TODO:
+    // - add redirect to Spotify Web
+    // - pass userId back to app
+    // - update userId state => changeUserId(userId)
+    // - update isLoggedIn state => changeIsLoggedIn(true)
+    // optional: add 'you're being redirected to spotify' page, 'failed login' page
 
-  return (
-    <SafeAreaView
-      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
-    >
-      <Button
-        title='Dummy Login'
-        onPress={() => changeIsLoggedIn(true)}
-        loadingProps={{ size: 'small', color: 'white' }}
-        buttonStyle={{
-          backgroundColor: 'rgba(111, 202, 186, 1)',
-          borderRadius: 5,
-        }}
-        titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
-        containerStyle={{
-          marginHorizontal: 100,
-          height: 50,
-          width: 200,
-          marginVertical: 10,
-        }}
-      />
-
-      <Button
-        title='SpotifyLogin'
-        onPress={() => {
-          apiLogin()
-        }}
-        loadingProps={{ size: 'small', color: 'white' }}
-        buttonStyle={{
-          backgroundColor: 'rgba(111, 202, 186, 1)',
-          borderRadius: 5,
-        }}
-        titleStyle={{ fontWeight: 'bold', fontSize: 23 }}
-        containerStyle={{
-          marginHorizontal: 100,
-          height: 50,
-          width: 200,
-          marginVertical: 10,
-        }}
-      />
-    </SafeAreaView>
-  )
+    return (
+        <View
+            style={{
+                flex: 1,
+            }}
+        >
+            <LinearGradient
+                colors={['#13151E', '#41BBC4']}
+                start={{ x: 0, y: 1 }}
+                end={{ x: 1, y: 0 }}
+                locations={[0.6, 1]}
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}
+            >
+                <Image
+                    style={{
+                        resizeMode: 'contain',
+                        position: 'absolute',
+                        left: 0,
+                        bottom: 0,
+                    }}
+                    source={require('../../../assets/loginIllustration.png')}
+                />
+                <Box
+                    style={{
+                        display: 'flex',
+                        flex: 1,
+                        padding: 'auto',
+                        alignItems: 'center',
+                        marginTop: 100,
+                    }}
+                >
+                    <BoldText style={{ fontSize: 24, color: 'white' }}>
+                        Share & Discover.
+                    </BoldText>
+                    <BoldText style={{ fontSize: 24, color: 'white' }}>
+                        Only on JamStream.
+                    </BoldText>
+                </Box>
+            </LinearGradient>
+            <Button
+                title='Log in'
+                onPress={() => {
+                    apiLogin()
+                }}
+                buttonStyle={{
+                    backgroundColor: COLORS.primary,
+                    borderRadius: 20,
+                    margin: 50,
+                    paddingVertical: 10,
+                }}
+                titleStyle={{
+                    fontWeight: 'bold',
+                    fontSize: 16,
+                    color: COLORS.dark,
+                }}
+                containerStyle={{
+                    width: '100%',
+                    position: 'absolute',
+                    bottom: 100,
+                    left: 0,
+                    shadowColor: '#0ff',
+                    shadowOpacity: 0.2,
+                    shadowRadius: 10,
+                }}
+            />
+        </View>
+    )
 }
